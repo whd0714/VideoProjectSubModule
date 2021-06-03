3cad230695286d9098a0ab99abd4efca89251e31
whd07 <whd0714@naver.com>
2021-06-03 16:04:19 +0900

    리액트 세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import NavBar from "./component/views/NavBar/NavBar";
+import LandingPage from "./component/views/LandingPage/LandingPage";
+import LoginPage from "./component/views/LoginPage/LoginPage";
+import RegisterPage from "./component/views/RegisterPage/RegisterPage";
+import VideoUploadPage from "./component/views/VideoUploadPage/VideoUploadPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <Router>
+            <NavBar></NavBar>
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/login" component={LoginPage} />
+                <Route exact path="/register" component={RegisterPage} />
+                <Route exact path="/video/upload" component={VideoUploadPage} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/component/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RegisterPage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default RegisterPage;
