5e87a8bbd2ed22adba7bdc2893896533964c6ca9
whd07 <jonghyun0714@gmail.com>
2021-06-18 14:54:55 +0900

    Subscription
    구독한 채널

--- src/component/views/SubscriptionPage/SubscriptionPage.js
+++ src/component/views/SubscriptionPage/SubscriptionPage.js
@@ -37,12 +37,33 @@ function SubscriptionPage(props) {
 
     const renderCard = videos.map((video, index)=>{
 
-        return <div  key={index}>
-            {video.videosDtos.map((v,i)=>{
-                return <Col key={i} xs={12}>
-                    <div>{v.title}</div>
-                </Col>
-            })}
+        return <div key={index} >
+            <div style={{padding:'1rem 0 0 1rem', fontWeight:'bold'}}>{video.creatorName}</div>
+            <br/>
+           <div style={{display:'flex'}}>
+               {video.videosDtos.map((v,i)=>{
+                   let minute = Math.floor(v.duration / 60);
+                   let second = Math.floor(v.duration - minute * 60);
+
+                   return <Col key={i} lg={6} md={8} xs={24}>
+
+                       <a href={`/video/${v.videoId}`}>
+                           <div style={{position:'relative'}}>
+                               <img style={{width:'300px', height:'240px'}} src={`http://localhost:8080/${v.thumbnailPath}`} alt=""/>
+                               <div className="duration">
+                                   <span>{minute} : {second}</span>
+                               </div>
+                           </div>
+                       </a>
+                       <br/>
+                       <Meta
+                           title={v.title}
+                           description = {v.description}
+                       />
+                       <span style={{marginLeft:'3rem'}}>{v.views} views</span> - <span>{moment(v.uploadDate).format("MMM Do YY")}</span>
+                   </Col>
+               })}
+           </div>
         </div>
 
 
@@ -78,7 +99,7 @@ function SubscriptionPage(props) {
         <div style={{width: '85%', margin: '3rem auto'}}>
             <Title level={2}>Subscription</Title>
             <hr/>
-            <Row gutter={[32, 16]} key="videos">
+            <Row style={{display:'flex', flexDirection:'column'}} gutter={[32, 16]} key="videos">
                 {renderCard}
             </Row>
         </div>
